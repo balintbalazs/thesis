9ff58aa5ddd3f7a9c83261e83a61ef1f0373eb34
Balint Balazs <balint.balazs@embl.de>
2017-09-28 10:29:00 +0200

    full traces done

--- figures/2_DualMouse/FPGA_traces.m
+++ figures/2_DualMouse/FPGA_traces.m
@@ -45,8 +45,42 @@ v0 = v0 + back_acc * delayAfter/2;
 v_back_dec = Voffset + v0*t_back_acc - t_back_acc.^2 * back_acc * 0.5;
 % y(61) = 0;
 % y(62:121) = -1 * y(60:1);
-plot(t_delay, [v_back_acc, v_back_dec])
+% plot(t_delay, [v_back_acc, v_back_dec])
 
 %%
 t = 0:tick:(exp + delayAfter - tick);
-plot(t, [v_acc, v_lin, v_dec, v_back_acc, v_back_dec])
+subplot(3,1,3)
+galvo = [v_acc, v_lin, v_dec, v_back_acc, v_back_dec];
+t = [t-exp-delayAfter, t, t+exp+delayAfter];
+%% galvo
+plot(t, repmat(galvo, 1,3))
+ylim([-0.6, 0.6])
+
+%% camera
+cam_exp = ones(size(t_exp));
+cam_delay = zeros(size(t_delay));
+subplot(3,1,1)
+plot(t, repmat([cam_exp, cam_delay], 1, 3))
+ylim([0, 1.2])
+
+%% laser digital
+l_acc = zeros(size(t_acc));
+l_lin = ones(size(t_lin));
+l_dec = l_acc;
+l_delay = zeros(size(t_delay));
+subplot(3,1,2)
+plot(t, repmat([l_acc, l_lin, l_dec, l_delay], 1, 3))
+ylim([0, 1.2])
+
+%%
+lines = [0, n_acc, n_acc+n_lin, exp, exp+delayAfter];
+colors = ['r', 'g', 'g', 'r', 'r'];
+for i =1:3
+    subplot(3,1,i)
+    pad = 2;
+    xlim([-pad, exp+delayAfter+pad])
+    hold on
+    for j = 1:length(lines)
+        plot([lines(j), lines(j)], [-2, 2], [colors(j),':'])
+    end
+end
